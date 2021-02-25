7efd1c63bd84b6e153ebd31d9616aa59702ae59b
Pauline <pauline.frare-pradal@laposte.net>
2021-02-25 20:59:08 +0100

    Demo: create error 404

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header() ?>
+
+    <h1>Page introuvable</h1>
+    <p>
+       Cette page n'existe pas
+    </p>
+
+<?php get_footer() ?>
+
+<!-- https://grafikart.fr/tutoriels/wordpress-hierarchy-1258#autoplay  5:28 -->
