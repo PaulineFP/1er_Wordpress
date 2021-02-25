d7fe1ce804ae81b095dbce4a694b61da46927b12
Pauline <pauline.frare-pradal@laposte.net>
2021-02-25 16:26:45 +0100

    Initial commit

---
+++ footer.php
@@ -0,0 +1,7 @@
+    </div>
+<?php wp_footer()?>
+<!--Plugin js-->
+<!--https://naver.github.io/egjs/   (carousel) -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/egjs-flicking/3.x.x/flicking.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <meta charset=utf-8" />
+    <?php wp_head();?>
+</head>
+<body>
+<div class="container">
