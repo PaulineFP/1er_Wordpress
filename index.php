a3f2571b5692b490ced44f24c590e4a7f1172c3f
Pauline <pauline.frare-pradal@laposte.net>
2021-02-26 15:42:38 +0100

    Demo: created search form

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
         <div class="col-sm-4">
             <div class="card">
                 <?php the_post_thumbnail('medium', ['class' => 'card-img-top', 'alt' => '', 'style'=> 'height: auto;'])?>
-                <img class="card-img-top" src="..." alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title"><?php the_title(); ?></h5>
                     <h6 class="card-subtitle mb-2 text-muted"><?php the_category() ?></h6>
